757716a937212707f74b1829be03523c851dd32c
eroepke <164393462+ethroepke@users.noreply.github.com>
2024-12-29 13:59:47 -0600

    validate email page and functionality complete

--- Frontend/app/src/main/java/com/example/androidexample/forgotpasswordActivity.java
+++ Frontend/app/src/main/java/com/example/androidexample/forgotpasswordActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidexample;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -7,25 +8,84 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
-import com.android.volley.DefaultRetryPolicy;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+/**
+ * This page is for user to enter their email to verify there is an email in database then get sent to screen to reset password
+ */
 
 public class forgotpasswordActivity extends AppCompatActivity {
 
+    private EditText emailInput;
+    private Button checkEmail;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forgotpassword);
+
+        emailInput = findViewById(R.id.emailInputForgotPassword);
+        checkEmail = findViewById(R.id.checkEmailForgotPassword);
+
+        //Toolbar setup
+        Toolbar toolbar = findViewById(R.id.toolBarForgotPassword);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Check Email");
+
+
+        /**
+         * Check Email button will check if user has valid email
+         * If valid then will send to reset password page
+         */
+        checkEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailInput.getText().toString().trim();
+
+                if(email.isEmpty()){
+                    Log.d("Email Submit Button", "Email field is empty");
+                    Toast.makeText(getApplicationContext(),"Please fill out email field", Toast.LENGTH_SHORT).show();
+                }
+
+                if(!email.isEmpty()) {
+                    if (validateCredentials(email)) {
+                        Log.d("Email Submit Button", "Email validated: email(" + email + ")");
+                        Intent intent = new Intent(forgotpasswordActivity.this, resetPasswordActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Log.d("Email Submit Button", "Email not validated: email(" + email + ")");
+                        Toast.makeText(getApplicationContext(), "Email incorrect", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+
+    //Setup for toolbar back button
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            Log.d("Check Email Back Button", "Back button from check email to login page");
+            Intent intent = new Intent(forgotpasswordActivity.this, loginActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //Test user email to be able to test reset password until we get API set up
+    private boolean validateCredentials(String email) {
+        return email.equals("testuser@testing.com");
+    }
 }
