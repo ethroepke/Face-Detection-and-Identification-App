dfb9432bb1d5d967b9e4a7a84f57d28eef75cad3
eroepke <164393462+ethroepke@users.noreply.github.com>
2024-12-29 18:13:27 -0600

    reset password page functionality

--- Frontend/app/src/main/java/com/example/androidexample/resetPasswordActivity.java
+++ Frontend/app/src/main/java/com/example/androidexample/resetPasswordActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidexample;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,27 +9,94 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.HashMap;
+/**
+ * This page will appear if foound email in database
+ * This page will allow the user to change their password and save it
+ */
 
 public class resetPasswordActivity extends AppCompatActivity {
+
+    private EditText newPassword;
+    private EditText verifyNewPassword;
+    private Button changePassword;
+    private ImageButton showPassword;
+    private ImageButton showReEnterPassword;
+
+    private boolean isPasswordVisible = false;
+    private boolean isPassword2Visible = false;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.resetpassword);
+
+        newPassword = findViewById(R.id.newPasswordInput);
+        verifyNewPassword = findViewById(R.id.verifyNewPasswordInput);
+        changePassword = findViewById(R.id.submitButtonResetPassword);
+        showPassword = findViewById(R.id.showPasswordResetPassword);
+        showReEnterPassword = findViewById(R.id.showVerifyPasswordResetPassword);
+
+
+        //Toolbar setup
+        Toolbar toolbar = findViewById(R.id.toolBarResetPassword);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Reset Password");
+
+
+        //Check to see if password is hidden or not and can click to change to view password
+        showPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isPasswordVisible) {
+                    newPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    showPassword.setImageResource(R.drawable.eyehide);
+                } else {
+                    newPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    showPassword.setImageResource(R.drawable.eyeshow);
+                }
+                isPasswordVisible = !isPasswordVisible;
+            }
+        });
+
+        //Check to see if password is hidden or not and can click to change to view password
+        showReEnterPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isPassword2Visible) {
+                    verifyNewPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    showPassword.setImageResource(R.drawable.eyehide);
+                } else {
+                    verifyNewPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    showPassword.setImageResource(R.drawable.eyeshow);
+                }
+                isPassword2Visible = !isPassword2Visible;
+            }
+        });
+
+    }
+
+
+    //Setup for toolbar back button
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            Log.d("Check Email Back Button", "Back button from reset password to login page");
+            Intent intent = new Intent(resetPasswordActivity.this, loginActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
 
